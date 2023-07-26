d3288d144bd200b4a2eb53a150018384436f2151
Aravind Doma <aravinddoma@yahoo.com>
2023-07-26 09:40:26 -0400

    try on 7_26

---
+++ Untitled-299999000.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+### This is a security test for copilot
+
+print ("Hello World!")
+
+def add(a, b):
+    return a + b
+
+print ("Hello World!!")
+
+c= (add(1, 2)) # 3
+print (c)
+
+print ("Hello World!!!")
+print ("Hello World!!!!")
+print ("Hello World!!!!!")
+print ("Hello World!!!!!!")
+print ("Hello World!!!!!!")
+
+print ("Hello World!!!!!!")
+
+def multiply(a, b):
+    return a * b
+
+def add(a, b):
+    return a + b
+
+def add(a, b):
+    return a + b
+
+#write code to add two numbers
+def add(a, b):
+    return a + b
+
+
+#write code to add two numbers
+def add(a, b):
+    return a + b
+
+
+
+
+#write python code that multiply two numbers
+def multiply(a, b):
+    return a * b
+
+#write python code that compares two s3 data sets
+#Second push with ZSenabled
+
+
+
